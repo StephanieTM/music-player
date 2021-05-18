e97eb432d00e804512a3c9559aad00bbd2dba658
Stephanie.Gao <tm1997@163.com>
2021-05-18 21:55:26 +0800

    feat: embed the audio

--- src/components/the-song/index.tsx
+++ src/components/the-song/index.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Button } from '@chakra-ui/react';
 
 export default function TheSong(): JSX.Element {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.play();
+      } else {
+        audioRef.current.pause();
+      }
+    }
+  }, [isPlaying]);
+
   return (
-    <div>The Song</div>
+    <div>
+      <Button onClick={() => setIsPlaying(!isPlaying)}>
+        {isPlaying ? 'Pause' : 'Play'}
+      </Button>
+      
+      <audio
+        ref={audioRef}
+        src="/assets/songs/the-song.mp3"
+        hidden
+      />
+    </div>
   );
 }
