80d1f47441ce8e46ec029a0d79200be70bb4b5fc
Stephanie.Gao <tm1997@163.com>
2021-05-19 13:31:21 +0800

    feat: remove local assets

--- webpack.common.js
+++ webpack.common.js
@@ -80,8 +80,6 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         { from: 'assets/images', to: 'assets/images' },
-        { from: 'assets/songs', to: 'assets/songs' },
-        { from: 'assets/lyrics', to: 'assets/lyrics' },
       ],
     }),
     new webpack.ProgressPlugin({ profile: false }),
