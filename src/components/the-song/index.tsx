6d8ed40298966d982aa6a8d4871f7dc48a053c52
Stephanie.Gao <tm1997@163.com>
2021-05-21 18:26:36 +0800

    feat: refactor Player

--- src/components/the-song/index.tsx
+++ src/components/the-song/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import LandPage from './LandPage';
 import Player from './Player';
 import LocalStore from './local-store';
+import './index.less';
 
 export default function TheSong(): JSX.Element {
   return (
     <LocalStore.Provider>
+      <LandPage />
       <Player />
     </LocalStore.Provider>
   );
